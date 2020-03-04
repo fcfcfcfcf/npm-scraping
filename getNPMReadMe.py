462cf3c05c22f05f6d917b1cb7de97e20c67600b
marshingjay <jam605@gmail.com>
2020-03-04 11:12:29 -0600

    added some threading code

--- getNPMReadMe.py
+++ getNPMReadMe.py
@@ -4,9 +4,12 @@ import re
 import random
 import hashlib
 import time
+import threading
+import logging
+import concurrent.futures
 
 
-big_dict_of_hashes {}
+big_dict_of_hashes = {}
 #function for getting the readme of a url
 #has to be valid npm url to package
 def get_readme(url):
@@ -27,8 +30,15 @@ def get_readme(url):
 
 print(get_readme('https://www.npmjs.com/package/loadsh'))
     
-   
+format = "%(asctime)s: %(message)s"
+logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+threads = list()
 
+for i in range(10):
+    logging.info("Creating and starting thread %d.", i)
+    new_thread = threading.Thread(target=get_readme, ars=(,)) #put urls in args when we have csv
+    threads.append(new_thread)
+    new_thread.start()
    
 
 
