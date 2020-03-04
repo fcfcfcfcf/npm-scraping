50d1f4ee46ffcf8fbe056a50240fbe2df2457dec
marshingjay <jam605@gmail.com>
2020-03-04 12:20:29 -0600

    parse csv

--- getNPMReadMe.py
+++ getNPMReadMe.py
@@ -7,6 +7,8 @@ import time
 import threading
 import logging
 import concurrent.futures
+import csv
+
 
 
 big_dict_of_hashes = {}
@@ -27,18 +29,30 @@ def get_readme(url):
 
 
 
-
+perpetrators = []
 print(get_readme('https://www.npmjs.com/package/loadsh'))
-    
-format = "%(asctime)s: %(message)s"
-logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
-threads = list()
+with open('npm_typosquatting_perpetrators.csv') as csvfile:
+    pkg_reader = csv.reader(csvfile, delimiter=',')
+    for row in pkg_reader:
+        new_perpetrator = []
+        for ind in range (len(row)):
+            if ind%2 == 0:
+                if row[ind] != '':
+                    new_perpetrator += [row[ind]]
+        perpetrators += [new_perpetrator]
+print(str(perpetrators))
 
-for i in range(10):
-    logging.info("Creating and starting thread %d.", i)
-    new_thread = threading.Thread(target=get_readme, ars=(,)) #put urls in args when we have csv
-    threads.append(new_thread)
-    new_thread.start()
+
+                
+# format = "%(asctime)s: %(message)s"
+# logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+# threads = list()
+
+# for i in range(10):
+#     logging.info("Creating and starting thread %d.", i)
+#     new_thread = threading.Thread(target=get_readme, ars=(,)) #put urls in args when we have csv
+#     threads.append(new_thread)
+#     new_thread.start()
    
 
 
